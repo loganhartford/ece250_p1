a59d77576eb7ea476b40148106c49d54d7152ecd
loganhartford <logan.hartford@outlook.com>
2024-10-03 19:43:01 -0400

    parsing

--- workStealing.cpp
+++ workStealing.cpp
@@ -12,6 +12,34 @@ int main()
         {
             cout << "success" << endl;
         }
+        else if (cmd == "SPAWN")
+        {
+            cout << "failure" << endl;
+        }
+        else if (cmd == "RUN")
+        {
+            cout << "failure" << endl;
+        }
+        else if (cmd == "SLEEP")
+        {
+            cout << "failure" << endl;
+        }
+        else if (cmd == "SHUTDOWN")
+        {
+            cout << "failure" << endl;
+        }
+        else if (cmd == "SIZE")
+        {
+            cout << "failure" << endl;
+        }
+        else if (cmd == "CAPACITY")
+        {
+            cout << "failure" << endl;
+        }
+        else if (cmd == "END")
+        {
+            return 0;
+        }
 
         return 0;
     }
